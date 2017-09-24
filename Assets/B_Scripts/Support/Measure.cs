259df55d697666e9adec83b014d5917b64a1ca16
kennebel <jac@kennebel.com>
2017-09-24 16:58:33 -0600

    Starting to get the math right...
    0.866, 1.732

---
+++ Assets/B_Scripts/Support/Measure.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Measure : MonoBehaviour
+{
+    #region Unity Events
+    void Awake()
+    {
+        var x = GetComponent<MeshRenderer>().bounds.extents.x;
+        var z = GetComponent<MeshRenderer>().bounds.extents.z;
+
+        Debug.Log(string.Format("Bounding box: {0} x {1}", x, z));
+    }
+    #endregion
+}
